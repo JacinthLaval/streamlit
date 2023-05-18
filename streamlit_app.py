56a6128398319f77543e2773d5ff27cbcb48ff52
JacinthLaval <47199523+JacinthLaval@users.noreply.github.com>
2023-05-18 14:15:10 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,7 +7,7 @@ st.title('Uber pickups in NYC')
 @st.cache_data
 def load_data(nrows):
          
- DATE_COLUMN = 'date/time'
+DATE_COLUMN = 'date/time'
 DATA_URL = ('https://s3-us-west-2.amazonaws.com/'
          'streamlit-demo-data/uber-raw-data-sep14.csv.gz')
 
